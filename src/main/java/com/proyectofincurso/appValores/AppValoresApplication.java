f6cf571b015e09b25f8a8e481aacc563e1a458ab
Jesús David <jesusdavid.guti@gmail.com>
2021-04-02 07:02:39 +0200

    Primer commit
    Primer commit

---
+++ src/main/java/com/proyectofincurso/appValores/AppValoresApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofincurso.appValores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppValoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppValoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectofincurso/appValores/AppValoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofincurso.appValores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppValoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

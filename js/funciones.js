f13533f8ea8bf724f67b96f8b0239b06784dd994
Jesús David <jesusdavid.guti@gmail.com>
2021-04-12 07:46:58 +0200

    Divisas
    CRUD divisa

---
+++ js/funciones.js
@@ -0,0 +1,53 @@
+
+//Validación del formulario de registros de datos de la divisa
+//Validará que los campos estén rellenos
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    var forms = document.getElementsByClassName('needs-validation');
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+//Envío de datos del formulario, método post.
+
+$('#formulario').on('submit', function(e) {
+  e.preventDefault();
+ // alert("entra");
+    registrar();
+
+});
+//Función que envía los datos de la divisa desde el formulario
+function registrar() {
+ 
+      var viaje = {
+          desc_corta:$("#inputDescripcionCorta").val(),
+          desc_larga:$("#inputDescripcionLarga").val(),
+          fec_inicio:$("#fechaInicio").val(),
+          fec_fin:$("#fechaFin").val(),
+          participantes:$("#inputParticipantes").val(),
+          medio_transporte:$("#inputTransporte").val()
+          
+      }
+
+      var dataJson = JSON.stringify(viaje);
+    //  alert(dataJson);
+      $.ajax({
+          url: "https://travellog2.herokuapp.com/api/viajes",
+          type: "post",
+          dataType: "json",
+          contentType: "application/json",
+          data: dataJson
+      });
+
+  
+  }
